20d6a7be62e2ee777448d05740e0bf7cd794b730
unn <nattapon@hotmail.com>
2021-01-08 15:24:15 +0700

    add sudoku codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Cell extends Component {
+  state = {
+    number: 5
+  };
+  render(){
+    return <div 
+      onClick={(e) => {
+        this.setState({
+          number: (this.state.number +1) % 5
+        });
+        //alert(this.state.number);
+      }} 
+      className="cell">{this.state.number !== 0 && this.state.number}</div>;
+  }
+}
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <div className="board">
+          <Cell number={3} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
