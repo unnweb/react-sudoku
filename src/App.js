4cc6627467fdb829a68a1ab354c313f51ce5d51c
unn <nattapon@hotmail.com>
2021-01-11 14:16:48 +0700

    create board

--- src/App.js
+++ src/App.js
@@ -10,35 +10,33 @@ class Cell extends Component {
     return <div 
       onClick={(e) => {
         this.setState({
-          number: (this.state.number +1) % 5
+          number: (this.props.number +1) % 5
         });
       }} 
-      className={`cell ${this.props.isInitial ? 'initial' : ''}`}>{this.state.number !== 0 && this.state.number}</div>;
+      className={`cell ${this.props.isInitial ? 'initial' : ''}`}>{this.props.number !== 0 && this.props.number}</div>;
   }
 }
 
+class Board extends Component {
+  state = {
+    board: [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+  };
+  render(){
+    return (
+      <div className="board">
+        {
+          this.state.board.map(row => 
+            row.map(number => <Cell number={number} />)
+        )}
+      </div>
+    );
+  }
+}
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <div className="board">
-          <Cell number={3} isInitial={true} />
-          <Cell number={4} isInitial />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-          <Cell number={4} />
-        </div>
+        <Board></Board>
       </div>
     );
   }
